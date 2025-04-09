678a70a3551caa3929a0f68c5e49f58722a0ebc1
HeidiSiao <heidi.siao@gmail.com>
2025-04-10 01:18:44 +0800

    refactor: all routes

---
+++ week6+/controllers/creditPackage.js
@@ -0,0 +1,111 @@
+const { dataSource } = require("../db/data-source");
+const logger = require("../utils/logger")("CreditPackage");
+const { packageFieldsCheck } = require("../utils/validUtils");
+const { customErr, correctRes } = require("../utils/resHandle");
+const { isUUID } = require("validator");
+
+const creditPackagesController = {
+
+  // GET 取得購買方案列表
+  async getPricingPlans (req, res, next) {
+    const packages = await dataSource.getRepository("CreditPackage").find({
+      select: ["id", "name", "credit_amount", "price"],
+    });
+    correctRes(res, packages);
+  },
+
+  // POST 新增購買方案
+  async postPricingPlans (req, res, next) {
+
+    // 保留未解構的 data 變數是必要的
+    // 在需要對整個物件進行操作時，例如驗證或檢查欄位
+    const data = req.body;
+    const { name, credit_amount, price } = data;
+    const invalidResult = packageFieldsCheck(data).filter(
+      (field) => field.isInvalid
+    );
+  
+    if (invalidResult.length > 0) {
+      const invalidMsg = invalidResult.map((field) => field.message);
+  
+      throw customErr(
+        400,
+        `欄位未填寫正確: ${invalidMsg.join(", ")}`
+      );
+    }
+  
+    const creditPackageRepo = dataSource.getRepository("CreditPackage");
+    const packageRecord = await creditPackageRepo.findOne({
+      where: { name },
+    });
+    if (packageRecord) {
+      throw customErr(409, "資料重複","conflict");
+    }
+  
+    const newPackage = creditPackageRepo.create({
+      name,
+      credit_amount,
+      price,
+    });
+    const savedPackage = await creditPackageRepo.save(newPackage);
+    correctRes(res, savedPackage);
+  },
+
+  // POST 新增使用者購買方案
+  async postUserPlan (req,res,next) {
+    // 中介層從JWT中提取並驗證使用者資訊，並存放在 req.user 中
+    // 獲取當前已驗證的使用者的 ID，確保後續操作的是該用戶自己的資料
+    const { id } = req.user;
+    // 某個特定資料的識別符
+    const { creditPackageId } = req.params;
+    const creditPackageRepo = dataSource.getRepository("CreditPackage");
+    const creditPackageRecord = await creditPackageRepo.findOne({
+      where: {
+        id: creditPackageId
+      }
+    });
+    
+    // 確認外面進來的購買方案，是否與資料庫內已存在的紀錄，為同一個id
+    if (!creditPackageRecord) {
+      logger.warn("ID錯誤");
+      throw customErr(400, "ID錯誤");
+    };
+  
+    // 建立已成立的課程購買紀錄
+    const creditPurchaseRepo = dataSource.getRepository("CreditPurchase");
+    const newPurchase = creditPurchaseRepo.create({
+      user_id: id,
+      credit_package_id: creditPackageId,
+      purchased_credits: creditPackageRecord.credit_amount,
+      price_paid: creditPackageRecord.price,
+      purchaseAt: new Date().toISOString()
+    });
+    const savedPurchase = await creditPurchaseRepo.save(newPurchase);
+  
+    // null 表示請求成功，但不回傳不必要的資訊，符合 RESTful API 設計指南
+    correctRes(res, null, 201);
+  },
+
+  // DELETE 刪除購買方案
+  async deletePlan (req, res, next){
+    const { creditPackageId } = req.params;
+  
+    // 查找資料庫前，先確認是否為有效的 UUID 格式
+    if (!isUUID(creditPackageId)) {
+      throw customErr(400,"ID錯誤");
+    }
+  
+    // 檢查資料庫是否確實刪除資料
+    const deleteCount = await dataSource
+      .getRepository("CreditPackage")
+      .delete(creditPackageId);
+  
+    if (deleteCount.affected === 0) {
+      throw customErr(404, "購買方案不存在");
+    }
+    correctRes(res, creditPackageId);
+  }
+};
+
+module.exports = creditPackagesController;
+
--- week6+/routes/users.js
+++ week6+/routes/users.js
@@ -1,175 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const bcrypt = require("bcrypt");
-const { dataSource } = require("../db/data-source");
-const logger = require("../utils/logger")("User");
-const { userFieldCheck, loginFieldCheck,  pwdRuleCheck, nameRuleCheck } = require("../utils/validUtils");
-const { customErr, correctRes } = require("../utils/resHandle");
-const { generateJWT } = require("../utils/jwtService");
 const isAuth = require("../middlewares/isAuth");
 const handleErrorAsync = require ("../utils/handleErrorAsync");
-
+const usersController = require ("../controllers/users");
 
 // POST 使用者註冊
-router.post("/signup", handleErrorAsync(async (req, res, next) => {
-  // 接收註冊資料
-  const data = req.body;
-  const { name, email, password } = data;
-
-  // 驗證必填欄位
-  const invalidMsg = userFieldCheck(data);
-  if (invalidMsg.length > 0) {
-    logger.warn("必填欄位未填寫正確");
-    throw customErr(400,`欄位未填寫正確: ${invalidMsg}`);
-  }
-
-  // 同時驗證密碼、姓名規則
-  const invalidName =  nameRuleCheck (name);
-  const invalidPwd = pwdRuleCheck(password);
-  const err = [];
-  if(invalidName) err.push(invalidName);
-  if(invalidPwd) err.push(invalidPwd);
-  if (err.length > 0) {
-    const errMsg = err.join("|");
-    logger.warn(`欄位未填寫正確: ${errMsg}`);
-    throw customErr(400,`欄位未填寫正確: ${errMsg}`);
-  };
-
-  // email是否與查詢資料庫結果重複
-  const userRepo = dataSource.getRepository("User");
-  const userRecord = await userRepo.findOneBy({ email });
-  if (userRecord) {
-    logger.warn("建立使用者錯誤: Email 已被使用");
-    throw customErr(409, "Email已被使用","conflict");
-  }
-
-  // 確認通過所有檢驗，建立新使用者資訊並儲存在資料庫
-  const saltRounds = 10;
-  const hashPassword = await bcrypt.hash(password, saltRounds);
-  const newUser = userRepo.create({
-    name,
-    email,
-    role: "USER",
-    password: hashPassword,
-  });
-  const savedUser = await userRepo.save(newUser);
-  logger.info("新建立的使用者ID:", savedUser.id);
-
-  // 包含敏感資訊，放入data物件前要先篩選過！
-  const filteredUserRes = {
-    user: {
-      id: savedUser.id,
-      name: savedUser.name,
-    },
-  };
-
-  correctRes(res, filteredUserRes, 201);
-}));
+router.post("/signup", handleErrorAsync(usersController.postSignup));
 
 // POST 使用者登入
-router.post("/login", handleErrorAsync(async (req, res, next ) => {
-  const { email, password } = req.body;
-  const invalidMsg = loginFieldCheck(email, password);
-
-  // 必填欄位驗證
-  if (invalidMsg.length > 0) {
-    logger.warn("必填欄位未填寫正確");
-    throw customErr(400, `必填欄位未填寫正確：${invalidMsg}`);
-  }
-  // 密碼欄位規則驗證
-  const isValidPwd = pwdRuleCheck(password);
-  if (isValidPwd) {
-    logger.warn(isValidPwd);
-    throw customErr(400, `必填欄位未填寫正確：${isValidPwd}`);
-  };
-
-  // 先檢查email是否存在，撈出指定欄位
-  const userRepo = dataSource.getRepository('User');
-  const userRecord = await userRepo.findOne({
-    select:['id','name','password'],
-    where: { email }
-  });
-
-  if (!userRecord) {
-    logger.warn("使用者不存在");
-    throw customErr(404, "使用者不存在");
-  };
-
-  // 再驗證密碼，bcrypt.compare()
-  const isMatch = await bcrypt.compare(password, userRecord.password);
-  if(!isMatch){
-    logger.warn("密碼輸入錯誤");
-    throw customErr(401, "密碼輸入錯誤","Unauthorized");
-  }
-  
-  // 登入成功，回傳JWT token給使用者
-  const token = generateJWT({
-    id: userRecord.id,
-    role: userRecord.role
-  });
-
-  const dataRes = {
-    token: token,
-    user: {
-      name: userRecord.name
-    } 
-  };
-
-  correctRes(res, dataRes, 201);
-}));
+router.post("/login", handleErrorAsync(usersController.postLogin));
 
 // GET 取得個人資料
-router.get("/profile", isAuth, handleErrorAsync(async (req,res,next) => {
-  
-  // body內沒有物件，已存放在req.user
-  // isAuth 已經確保了 id 的有效性
-  const { id } = req.user;
-  const userRecord = await dataSource.getRepository("User")
-  .findOne ({
-    select: ['email','name'],
-    where: { id }
-  });
-
-  const dataRes = {
-    user: userRecord
-  }
-  correctRes(res, dataRes, 200);
-
-}));
+router.get("/profile", isAuth, handleErrorAsync(usersController.getPersonalInfo));
 
 // PUT 更新個人資料 (要token權限)
-router.put("/profile", isAuth, handleErrorAsync(async (req,res,next) => {
-  const { id } = req.user;
-    const { name } = req.body;
-    const invalidMsg = nameRuleCheck(name);
-    if (invalidMsg) {
-      logger.warn(`欄位未填寫正確: ${invalidMsg}`);
-      throw customErr(400, `欄位未填寫正確：${invalidMsg}`);
-    };
-
-    // 檢查使用者名稱是否有修改，沒修改的話不需要更新資料庫
-    const userRepo = dataSource.getRepository("User");
-    const userRecord = await userRepo.findOneBy({ id });
-    if (userRecord.name === name ) {
-      logger.warn("使用者名稱未變更");
-      throw customErr(400, "使用者名稱未變更");
-    };
-
-    // 確認使用者有修改，資料庫進行更新
-    const updatedUser = await userRepo.update(
-    // 條件先放，再更新的欄位
-       { id }, { name }
-    );
-
-    // 資料庫是否成功更新
-    if (updatedUser.affected === 0 ) {
-      logger.warn("更新使用者失敗");
-      throw customErr(400, "更新使用者失敗");
-    };
-
-    correctRes(res);
-}));
+router.put("/profile", isAuth, handleErrorAsync(usersController.putPersonalInfo));
 
 
 
